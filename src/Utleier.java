2af57a8845b062454da36bc313598d8fd4a19e3a
kristofferosen <kristofferosen@gmail.com>
2014-04-02 14:39:31 +0200

    legger til ting i utleier

--- src/Utleier.java
+++ src/Utleier.java
@@ -1,12 +1,51 @@
 /**
  * Created by mac on 02.04.14.
  */
-public class Utleier extends Person
+public class Utleier extends Person implements serializable
 {
+    private String firma;
+    public Utleier neste;
+    public Boligliste boliger;
+
     public Utleier(String n, String a, String t, String e, String f, )
     {
         super(n, a, t, e);
-
+        firma = f;
     }
 
+    public String getNavn()
+    {
+        return super.getNavn();
+    }
+
+    public String getEmail()
+    {
+        return super.getEmail();
+    }
+
+
+
+    public String toString()
+    {
+        return super.toString() + "\nFirma: " + firma;
+    }
+
+
+    //klasser som kan være nødvendige:
+    /*
+    public Bolig harBolig(Bolig b)
+    {
+        return Bolig;
+    }
+    public Bolig harBolig(String bolignummer)
+    {
+        return Bolig;
+    }
+
+    public Boligliste getBoliger()
+    {
+        return boliger;
+    }
+
+    */
 }
