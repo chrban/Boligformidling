f24f476d5df3aa5dd1f0fe899f624ac8771f5fca
kristofferosen <kristofferosen@gmail.com>
2014-04-04 12:44:50 +0200

    boligsøker

---
+++ src/Boligsøker.java
@@ -0,0 +1,28 @@
+import java.io.Serializable;
+
+/**
+ * Created by Kristoffer on 04.04.2014.
+ */
+public class Boligsøker extends Person implements Serializable
+{
+    int[] Krav;
+
+    public Person (){}
+
+    public Person(String n, String a, String t, String e, int[] k )
+    {
+        super(n, a, t, e);
+        Krav = k;
+    }
+
+   public int[] getKrav()
+   {
+       return Krav;
+   }
+
+   public String toString()
+   {
+       return super.toString() + " pikk";
+   }
+
+}
