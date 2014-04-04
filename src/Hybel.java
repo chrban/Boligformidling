40d462a7751e9783ccb6a29fcc20be62f0453539
emop2109 <e.oppegaard@gmail.com>
2014-04-04 12:25:24 +0200

    Hybel

--- src/Hybel.java
+++ src/Hybel.java
@@ -1,13 +1,11 @@
 import java.io.Serialiazble;
 
 public class Hybel implements Serializable {
-    private int etasje;
     private int badDelesMed;
     private int kjøkkenDelesMed;
 
     public Hyble(String ad, int b, int r, int by, int u, int e, int bad, int kj) {
         super(ad, b, r, by, u);
-        etasje = e;
         badDelesMed= bad;
         kjøkkenDelesMed = kj;
     }
