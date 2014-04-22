edbc763db2b6b454532131455097abfe33b3c65f
kristofferosen <kristofferosen@gmail.com>
2014-04-22 10:20:12 +0200

    boligliste? generisk? hva ta hells

---
+++ src/Boligliste.java
@@ -0,0 +1,13 @@
+import java.util;
+
+/**
+ * Created by Kristoffer on 16.04.2014.
+ */
+public class Boligliste {
+    private SortedSet<> boliger;
+
+    private Boligliste()
+    {
+        boliger = new TreeSet<? extends Bolig>; // er dette riktig?
+    }
+}
