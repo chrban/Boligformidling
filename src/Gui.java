32c55daf69bb26852ba83000a007efa386d3c28d
chrban <christer@bang.is>
2014-04-24 22:02:34 +0200

    Laget en knapp, because i can

--- src/Gui.java
+++ src/Gui.java
@@ -9,6 +9,8 @@ public class Gui extends JFrame
     private JTabbedPane fane = new JTabbedPane();
     private GridBagLayout layout = new GridBagLayout();
     private GridBagConstraints c = new GridBagConstraints();
+    private JButton knapp1, knapp2, knapp3;
+    private JTextField tekst1, tekst2, tekst3;
 
     public Gui()
     {
@@ -22,6 +24,18 @@ public class Gui extends JFrame
 
         JPanel panel1 = new JPanel(layout);
         fane.addTab("Ny utleier", null, panel1, "Registrering");
+
+
+        knapp1 = new JButton("Knapp 1");
+            c.weightx = 0.0;
+            c.gridx = 0;
+            c.gridy = 0;
+        panel1.add(knapp1, c);
+
+
+
+
+
 
 
         // FANA #2 - Boligsøker
