0d103d51ae02b9f8553d0b58ec35914740048657
kristofferosen <kristofferosen@gmail.com>
2014-04-24 13:11:57 +0200

    kravarray, mekket den, svai

--- src/Boligsøker.java
+++ src/Boligsøker.java
@@ -7,90 +7,34 @@ public class Boligsøker extends Person implements Serializable
 {
     int[] Krav;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     Boligsøker neste;
 
-    public Person (){}
+    // datafelt for krav
+    int by, rom, minPris, maxPris, parkering, antEtasjer, kjeller, minTomt,
+        maxTomt, heis, balkong, delerBadMed, delerKjøkkenMed;
 
-    public Person(String n, String a, String t, String e, int[] k )
+
+    public Boligsøker (){}
+
+    public Boligsøker(String n, String a, String t, String e, int b, int r, int
+                  map, int mip, int p, int ae, int k, int mit, int mat, int h,
+                  int blkng, int dbm, int dkm )
     {
         super(n, a, t, e);
-        Krav = k;
+
+        // krav
+        by = b; rom = r; minPris = mip; maxPris = map; parkering = p; antEtasjer = ae;
+        kjeller = k; minTomt = mit; maxTomt = mat; heis = h; balkong = blkng;
+        delerBadMed = dbm; delerKjøkkenMed= dkm;
     }
 
    public int[] getKrav()
    {
+       Krav[0] = by;        Krav[1] = rom;         Krav[2] = minPris;  Krav[3] = maxPris;
+       Krav[4] = parkering; Krav[5] = antEtasjer;  Krav[6] = kjeller;  Krav[7] = minTomt;
+       Krav[8] = maxTomt;   Krav[9] = heis;        Krav[10] = balkong; Krav[11] = delerBadMed;
+       Krav[12] = delerKjøkkenMed;
+
        return Krav;
    }
 
@@ -100,3 +44,21 @@ public class Boligsøker extends Person implements Serializable
    }
 
 }
+
+//Visualisering av kravArray:
+// krav = {by, rom, minPris, maksPris, prakering, antEtasjer, kjeller, minTomt, maxTomt, heis, balkong, delerBadMed, delerKjøkkenMed}
+    /*
+    0 by
+    1 rom
+    2 minPris
+    3 maxPris
+    4 parkering
+    5 antEtasjer
+    6 kjeller
+    7 minTomt
+    8 maxTomt
+    9 heis
+    10 balkong
+    11 delerBadMed
+    12 delerKjøkkenMed
+    */
