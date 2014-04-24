b7160b14236953a2e2f0ff89d744534cb4133f1d
chrban <christer@bang.is>
2014-04-24 14:27:05 +0200

    GUI start

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by mac on 01.04.14.
  */
@@ -7,6 +10,18 @@ public class
 {
     public static void main (String[]args)
     {
+        EventQueue.invokeLater(new Runnable() {
+            public void run()
+            {
+                Gui vindu = new Gui();
+                vindu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                vindu.setVisible(true);
+
+
+            }
+        });
+
+
         System.out.println("HELLOE PIKKK!");
     }
 }
