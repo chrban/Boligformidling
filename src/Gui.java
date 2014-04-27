ec359e2bb1dcb4619f9e8774b457373d1bce094f
chrban <christer@bang.is>
2014-04-27 23:07:02 +0200

    Lagt til noe kompnenter

--- src/Gui.java
+++ src/Gui.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+
 
 /**
  * Created by mac on 02.04.14.
@@ -10,7 +12,9 @@ public class Gui extends JFrame
     private GridBagLayout layout = new GridBagLayout();
     private GridBagConstraints c = new GridBagConstraints();
     private JButton knapp1, knapp2, knapp3;
-    private JTextField tekst1, tekst2, tekst3;
+    private JTextField fornavn, etternavn, adresse, mail;
+    private JTextArea infonavn;
+
 
     public Gui()
     {
@@ -24,13 +28,65 @@ public class Gui extends JFrame
 
         JPanel panel1 = new JPanel(layout);
         fane.addTab("Ny utleier", null, panel1, "Registrering");
+            c.insets = new Insets(5,5,5,5);
+
+        // border
+        Border ramme = BorderFactory.createLineBorder(Color.BLACK);
+        panel1.setBorder(ramme);
 
 
-        knapp1 = new JButton("Knapp 1");
-            c.weightx = 0.0;
+        //Inndatafelt
             c.gridx = 0;
             c.gridy = 0;
-        panel1.add(knapp1, c);
+        panel1.add(new JLabel("Fornavn: "), c);
+
+        fornavn = new JTextField();
+            c.gridx = 1;
+            c.gridy = 0;
+            c.fill = GridBagConstraints.BOTH;
+        panel1.add(fornavn, c);
+        fornavn.setBorder(ramme);
+
+
+            c.gridx = 0;
+            c.gridy = 1;
+        panel1.add(new JLabel("Etternavn: "), c);
+
+        etternavn = new JTextField();
+            c.gridx = 1;
+            c.gridy = 1;
+            c.fill = GridBagConstraints.HORIZONTAL;
+        panel1.add(etternavn, c);
+
+
+            c.gridx = 0;
+            c.gridy = 2;
+        panel1.add(new JLabel("Adresse: "), c);
+
+        adresse = new JTextField();
+            c.gridx = 1;
+            c.gridy = 2;
+            c.fill = GridBagConstraints.HORIZONTAL;
+        panel1.add(adresse, c);
+
+            c.gridx = 0;
+            c.gridy = 3;
+        panel1.add(new JLabel("Mail: "), c);
+
+        mail = new JTextField();
+            c.gridx = 1;
+            c.gridy = 3;
+            c.fill = GridBagConstraints.HORIZONTAL;
+        panel1.add(mail, c);
+
+
+
+
+
+
+
+
+
 
 
 
@@ -42,6 +98,9 @@ public class Gui extends JFrame
 
         JPanel panel2 = new JPanel(layout);
         fane.addTab("Ny boligsøker", null, panel2, "registrerer boligsøker");
+
+
+
 
 
 
