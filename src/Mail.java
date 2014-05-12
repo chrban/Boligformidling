f1a3937d581d9558ab13e8fb56177651f4448c7b
emop2109 <e.oppegaard@gmail.com>
2014-05-12 13:47:58 +0200

    kommentering

--- src/Mail.java
+++ src/Mail.java
@@ -3,11 +3,18 @@ import javax.mail.*;
 import javax.mail.internet.*;
 import javax.swing.JOptionPane;
 import javax.activation.*;
-
+/*
+Filen inneholder metoder for å sende mail med informasjon om boliger til boligsøkere
+Skrevet av: Emil, s198772
+Sist oppdatert: 09/05/2014
+ */
 public class Mail
 {
+    /*
+    Sender en mail med informasjon om en bolig til en gitt epostaddresse. Variablene i parameterlista er informasjon om
+    boligen vi har tenkt å innformere boligsøkeren om.
+     */
     public void sendMail(String epost, String navn, String adresse, String sted, int pris){
-        String to = epost;
         final String from = "kamera@bang.is";
         final String brukernavn = "bangis5";
         final String password = "Svarten1975";
@@ -27,7 +34,7 @@ public class Mail
         try{
             MimeMessage message = new MimeMessage(session);
             message.setFrom( new InternetAddress(from));
-            message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(epost));
             message.setSubject("BoligMatch");
             message.setText("Hei " + navn + ".\nVi har funnet en bolig som vi tror passer for deg: \nBolig: "
             + adresse + "\n" + sted + "\n" + pris + "\nHvis denne passer for deg, send oss en tilbakemedlig\n Mvh. BoligFormidling.");
@@ -39,4 +46,4 @@ public class Mail
             throw new RuntimeException(mex);
         }
     }
-}
+}//end of class Mail
--- src/UtleierListe.java
+++ src/UtleierListe.java
@@ -1,35 +1,41 @@
 import java.util.*;
 import java.io.*;
 import javax.swing.JOptionPane;
-
+/*
+Filen inneholder metoder for å opprette og gjøre opperasjoner på en liste med utleier objekter
+Skrevet av: Emil, s198772
+Sist oppdatert:
+ */
 public class UtleierListe implements Serializable{
     private Boligliste boliger;
-
+    //Konstruktøren har kun en variable i parameterlista, den brukes i en av metodene
     public UtleierListe(Boligliste b){
         boliger = b;
     }
 
     private List<Utleier> liste = new LinkedList<>();
-
+    //setter inn et nytt Utleier objekt i lista
     public void settInn( Utleier u ){
         liste.add( u );
         sorter();
     }
+    //fjerner et Utleier objekt med hensyn på fornavn og etternavn
     public boolean fjernUtleier( String navn, String etternavn ){
         Iterator<Utleier> iterator = liste.iterator();
         while ( iterator.hasNext() ){
             Utleier fjern = iterator.next();
             if(fjern.getFornavn().equals(navn) && fjern.getEtternavn().equals(etternavn)){
-                liste.remove(fjern);
-                return true;
+                return liste.remove(fjern);
             }
         }
         return false;
     }
+    //metoden sorter lista etter reglene som blir gitt i UtleierSammenlikner klassen
     public void sorter(){
         Collections.sort(liste, new UtleierSammenlikner());
     }
-
+    //I GUI blir det brukt JTable for å vise informasjon, det kreves at informasjonen kommer i form av en Stringarray
+    //denne moten returnerer en slik Stringarray
     public String[][] tilTabell()
     {
         String[][] ut = new String[liste.size()][6];
@@ -45,6 +51,7 @@ public class UtleierListe implements Serializable{
         }
         return ut;
     }
+    //Samme metode som over, bare at den tar med id'n til utleieren
     public String[][] tilTabellMedId()
     {
         String[][] ut = new String[liste.size()][7];
@@ -60,7 +67,7 @@ public class UtleierListe implements Serializable{
         }
         return ut;
     }
-
+    //Vanlig toString metode for utleierliste
     public String toString(){
         sorter();
         String utleiere = "";
@@ -70,6 +77,7 @@ public class UtleierListe implements Serializable{
         }
         return utleiere;
     }
+    //Finner en gitt utleier med hensyns på id'n som kommer i parameterlista
     public Utleier getUtleier(String i){
         System.out.println("leter etter boligsøker");
         Iterator<Utleier> it = liste.iterator();
@@ -81,12 +89,14 @@ public class UtleierListe implements Serializable{
         }
         return null;
     }
+    //Sjekker om en utleier har boliger registrert på seg
     public boolean harBoliger(Utleier b){
         if(boliger.harBolig(b))
             return true;
         else
             return false;
     }
+    //Finner id'n til utleier med hensyn på fornavn og etternavn
     public String finnID(String n, String f){
         Iterator<Utleier> it = liste.iterator();
         Utleier finn = null;
@@ -97,4 +107,4 @@ public class UtleierListe implements Serializable{
         }
         return null;
     }
-}
+}//end of class UtleierListe
